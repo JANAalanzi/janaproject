0f257e6e3101a3d8e6b5fee71b9d1e14de4a3db7
janaa <janaabdullh59@gmail.com>
2023-11-13 20:06:24 +0200

    phase one of branch_2 complete

--- banner2.py
+++ banner2.py
@@ -1,9 +1,8 @@
-banner2 = 'My other cool banner'
-
-name = raw_input("Hi. What's your name?")
+name = "jana"
+age = 21
 
 print name
 
-print "hi, " + name + " my banner is " + banner2
+print "hi, " + name 
 
 raw_input("\n\nPress the enter key to exit.")

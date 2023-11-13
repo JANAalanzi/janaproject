1d3a75110353837834850e5b5cd3cebfc5d9883d
janaa <janaabdullh59@gmail.com>
2023-11-13 18:38:20 +0200

    phase one of banner comlete

---
+++ banner2.py
@@ -0,0 +1,9 @@
+banner2 = 'My other cool banner'
+
+name = raw_input("Hi. What's your name?")
+
+print name
+
+print "hi, " + name + " my banner is " + banner2
+
+raw_input("\n\nPress the enter key to exit.")
